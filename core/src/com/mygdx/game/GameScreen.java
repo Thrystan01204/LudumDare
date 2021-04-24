456e4460e171b1f655e458e4048048e9b87c8826
smallcluster <small.cluster@gmail.com>
2021-04-24 16:03:09 +0400

    setup des screens

---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,58 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen implements Screen {
+
+    final BananaPeelSplit game;
+    OrthographicCamera camera;
+
+    public GameScreen(final BananaPeelSplit game){
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 1280, 720);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(02, 0, 0.2f, 1);
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+
+        game.batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
